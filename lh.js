adcc97592fffe0c3e0fbb26a7913206c11ac3119
Andy Laurenson <andrew.laurenson@bjss.com>
2021-03-24 11:26:06 +0000

    average reports run

--- lh.js
+++ lh.js
@@ -5,6 +5,12 @@ const url = require("url");
 const fs = require("fs");
 const glob = require("glob");
 const path = require("path");
+const { run } = require("lighthouse/lighthouse-core/runner");
+const G = require("glob");
+const { sync } = require("glob");
+const averageObject = { was: [], now: [] }
+var dirName;
+
 
 const launchChromeAndRunLighthouse = url => {
 
@@ -14,6 +20,7 @@ const launchChromeAndRunLighthouse = url => {
             onlyCategories: ['performance'],
         };
         return lighthouse(url, opts).then(results => {
+
             return chrome.kill().then(() => {
                 return {
                     js: results.lhr,
@@ -27,6 +34,7 @@ const launchChromeAndRunLighthouse = url => {
 const getContents = pathStr => {
     const output = fs.readFileSync(pathStr, "utf8", (err, results) => {
         return results;
+
     });
     return JSON.parse(output);
 };
@@ -55,52 +63,59 @@ const compareReports = (from, to) => {
                 to["audits"][auditObj].numericValue
             );
 
-            const log = (() => {
-                if (Math.sign(percentageDiff) === 1) {
-                    logColor = "\x1b[31m";
-                    return `${percentageDiff.toString().replace("-", "") + "%"} slower`;
-                } else if (Math.sign(percentageDiff) === 0) {
-                    return "unchanged";
-                } else {
-                    logColor = "\x1b[32m";
-                    return `${percentageDiff.toString().replace("-", "") + "%"} faster`;
-                }
-            })();
+            var nowRead;
+            var fromRead;
+            var title;
 
-            console.log(logColor, `${from["audits"][auditObj].title} is ${log}`);
-            console.log(logColor,
-                'Was: ' + Math.round((from["audits"][auditObj].numericValue + Number.EPSILON) * 100) / 100,
-                '\n Now: ' + Math.round((to["audits"][auditObj].numericValue + Number.EPSILON) * 100) / 100
-            );
 
-            console.log("\x1b[37m", '');
+            const log = displayHelper(percentageDiff);
+            fromRead = (Math.round((from["audits"][auditObj].numericValue + Number.EPSILON) * 100) / 100)
+            nowRead = (Math.round((to["audits"][auditObj].numericValue + Number.EPSILON) * 100) / 100)
+            title = (`${from["audits"][auditObj].title} is ${log}`)
+
+            if (!argv.average) {
+                console.log(logColor, title);
+                console.log(logColor, 'Was: ' + fromRead);
+                console.log(logColor, 'Now: ' + nowRead);
+                console.log("\x1b[37m", '');
+            }
+
+            averageObject.was.push(fromRead);
+            averageObject.now.push(nowRead);
+
+            fs.appendFile("runLogResults.json", title + '\n' + fromRead + '\n' + nowRead + '\n', err => {
+                if (err) throw err;
+            });
         }
     }
 };
 
-if (argv.from && argv.to) {
-    compareReports(
-        getContents(argv.from + ".json"),
-        getContents(argv.to + ".json")
-    );
-} else if (argv.url) {
+const runCheck = () => {
     const urlObj = new URL(argv.url);
-    let dirName = urlObj.host.replace("www.", "");
+    dirName = urlObj.host.replace("www.", "");
     if (urlObj.pathname !== "/") {
         dirName = dirName + urlObj.pathname.replace(/\//g, "_");
     }
 
-    if (!fs.existsSync('reports/' + dirName)) 
-    {
-        if (!fs.existsSync('reports')) 
-        {
+    if (!fs.existsSync('reports/' + dirName + '/single/')) {
+        if (!fs.existsSync('reports')) {
             fs.mkdirSync('reports')
         }
-        fs.mkdirSync('reports/' + dirName);
+        if (!fs.existsSync('reports/' + dirName)) {
+            fs.mkdirSync('reports/' + dirName)
+        }
+        if (!fs.existsSync('reports/' + dirName + '/single/')) {
+            fs.mkdirSync('reports/' + dirName + '/single/')
+        }
+    }
+
+    if (argv.average && !fs.existsSync('reports/' + dirName + '/average')) {
+        fs.mkdirSync('reports/' + dirName + '/average')
+
     }
 
     launchChromeAndRunLighthouse(argv.url).then(results => {
-        const prevReports = glob('reports/' + `${dirName}/*.json`, {
+        const prevReports = glob('reports/' + `${dirName}/single/*.json`, {
             sync: true
         });
 
@@ -117,20 +132,167 @@ if (argv.from && argv.to) {
             const recentReport = new Date(max).toISOString();
 
             const recentReportContents = getContents(
-                'reports/' + dirName + "/" + recentReport.replace(/:/g, "_") + ".json"
+                'reports/' + dirName + "/single/" + recentReport.replace(/:/g, "_") + ".json"
             );
 
             compareReports(recentReportContents, results.js);
         }
 
         fs.writeFile(
-            `reports/${dirName}/${results.js["fetchTime"].replace(/:/g, "_")}.json`,
+            `reports/${dirName}/single/${results.js["fetchTime"].replace(/:/g, "_")}.json`,
             results.json,
             err => {
                 if (err) throw err;
             }
         );
     });
-} else {
+};
+
+let average = [1, 2, 3]
+runAverage = async () => {
+    for (const run of average) {
+        await new Promise(resolve => setTimeout(resolve, 70000)).then(runCheck());
+    }
+    displayAverage();
+};
+
+
+if (argv.from && argv.to) {
+    compareReports(
+        getContents(argv.from + ".json"),
+        getContents(argv.to + ".json")
+    );
+}
+else if (argv.url) {
+    if (argv.average) {
+        runAverage();
+        console.log('\x1b[33m', '');
+        console.log('\x1b[33m', '...... ...... ...... ...... ');
+        console.log('\x1b[33m', 'Running Average report .... ');
+        console.log('\x1b[33m', 'Please hold .... ..... .... ');
+        console.log('\x1b[33m', '...... ...... ...... ...... ');
+
+    }
+    else {
+        runCheck();
+        console.log('\x1b[33m', '');
+        console.log('\x1b[33m', '...... ...... ...... ...... ');
+        console.log('\x1b[33m', 'Running one off report .... ');
+        console.log('\x1b[33m', '...... ...... ...... ...... ');
+
+    }
+}
+
+else {
     throw "You haven't passed a URL to Lighthouse, see README";
+}
+
+function displayHelper(percentageDiff) {
+    return (() => {
+        if (Math.sign(percentageDiff) === 1) {
+            logColor = "\x1b[31m";
+            return `${percentageDiff.toString().replace("-", "") + "%"} slower`;
+        } else if (Math.sign(percentageDiff) === 0) {
+            return "unchanged";
+        } else {
+            logColor = "\x1b[32m";
+            return `${percentageDiff.toString().replace("-", "") + "%"} faster`;
+        }
+    })();
+}
+
+displayAverage = async () => {
+
+    const averageChangeNow = { fcp: 0, lcp: 0, tbt: 0, cls: 0, tti: 0 };
+    var recentReportPathStr;
+    var recentAverageReport;
+
+    const prevAverageReports = glob('reports/' + `${dirName}/average/*.json`, {
+        sync: true
+    });
+
+    if (prevAverageReports.length) {
+        dates = [];
+        for (report in prevAverageReports) {
+
+            var dateParts = prevAverageReports[report].replace(dirName, '').replace('reports//average/', '');
+
+            var timeOfReport = prevAverageReports[report].split(',')[1];
+            timeOfReport = timeOfReport.split('.')[0].replace(/_/g, ':')
+            dateParts = dateParts.split('_');
+            dateParts[0] = dateParts[0].replace('/', '');
+            dateParts[2] = dateParts[2].split(',')[0];
+
+            var formattedTime = (dateParts[2] + '-' + dateParts[1].toString() + '-' + dateParts[0]) + 'T' + timeOfReport + 'Z'
+            var dateObject = new Date(formattedTime);
+
+            dates.push(
+                new Date(dateObject)
+            );
+        }
+
+        const maxAverage = dates.reduce(function (a, b) {
+            return Math.max(a, b);
+        });
+        recentAverageReport = new Date(maxAverage).toLocaleString();
+        recentReportPathStr = 'reports/' + dirName + "/average/" + recentAverageReport.replace(' ', '').replace(/:/g, "_").replace(/\//g, "_") + ".json";
+        const readRecentAverageReport = getContents(recentReportPathStr);
+
+        var nowValues = {};
+
+        nowValues = Object.entries(averageChangeNow)
+
+        let average = 0;
+        const calcPercentageDiff = (from, to) => {
+            const per = ((to - from) / from) * 100;
+            return Math.round(per * 100) / 100;
+        };
+
+        averageChangeNow.fcp = ((averageObject.now[0] + averageObject.now[5] + averageObject.now[10]) / 3);
+        averageChangeNow.lcp = ((averageObject.now[1] + averageObject.now[6] + averageObject.now[11]) / 3);
+        averageChangeNow.tbt = ((averageObject.now[2] + averageObject.now[7] + averageObject.now[12]) / 3);
+        averageChangeNow.cls = ((averageObject.now[3] + averageObject.now[8] + averageObject.now[13]) / 3);
+        averageChangeNow.tti = ((averageObject.now[4] + averageObject.now[9] + averageObject.now[14]) / 3);
+
+        console.log("\x1b[37m", '');
+        console.log('Average for FCP was: ' + Math.round(readRecentAverageReport.fcp * 100) / 100);
+        console.log('Average for FCP now: ' + Math.round(averageChangeNow.fcp * 100) / 100);
+        average = displayHelper((calcPercentageDiff(readRecentAverageReport.fcp, averageChangeNow.fcp)))
+        console.log(logColor, 'Changed: ' + average)
+
+        console.log("\x1b[37m", '');
+        console.log('Average for LCP was: ' + Math.round(readRecentAverageReport.lcp * 100) / 100);
+        console.log('Average for LCP now: ' + Math.round(averageChangeNow.lcp * 100) / 100);
+        average = displayHelper((calcPercentageDiff(readRecentAverageReport.lcp, averageChangeNow.lcp)))
+        console.log(logColor, 'Changed: ' + average)
+
+        console.log("\x1b[37m", '');
+        console.log('Average for TBT was: ' + Math.round(readRecentAverageReport.tbt * 100) / 100);
+        console.log('Average for TBT now: ' + Math.round(averageChangeNow.tbt * 100) / 100);
+        average = displayHelper((calcPercentageDiff(readRecentAverageReport.tbt, averageChangeNow.tbt)))
+        console.log(logColor, 'Changed: ' + average)
+
+        console.log("\x1b[37m", '');
+        console.log('Average for CLS was: ' + Math.round(readRecentAverageReport.cls * 100) / 100);
+        console.log('Average for CLS now: ' + Math.round(averageChangeNow.cls * 100) / 100);
+        average = displayHelper((calcPercentageDiff(readRecentAverageReport.cls, averageChangeNow.cls)))
+        console.log(logColor, 'Changed: ' + average)
+
+        console.log("\x1b[37m", '');
+        console.log('Average for TTI was: ' + Math.round(readRecentAverageReport.tti * 100) / 100);
+        console.log('Average for TTI now: ' + Math.round(averageChangeNow.tti * 100) / 100);
+        average = displayHelper((calcPercentageDiff(readRecentAverageReport.tti, averageChangeNow.tti)))
+        console.log(logColor, 'Changed: ' + average)
+
+    }
+
+    var currentDateTime = new Date();
+    fs.writeFile(
+        `reports/${dirName}/average/${currentDateTime.toLocaleString().replace(/:/g, "_").replace(/\//g, "_").replace(' ', '')}.json`,
+        JSON.stringify(averageChangeNow)
+        ,
+        err => {
+            if (err) throw err;
+        }
+    );
 }
